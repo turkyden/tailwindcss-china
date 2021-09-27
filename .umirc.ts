6f0b403f5042ea35a74175abbbaee62a65d8d2fa
“turkyden” <wj871287@gmail.com>
2021-09-27 18:25:19 +0800

    feat: finish home pages

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  title: 'TailwindCSS 中国社区',
+  title: 'TailwindCSS 中文社区',
+  metas: [
+    {
+      name: 'keywords',
+      content: 'TailwindCSS, 中文社区, 模板, 组件',
+    },
+    {
+      name: 'description',
+      content: 'TailwindCSS 中文社区',
+    },
+  ],
+  // ssr: {},
+  // exportStatic: {},
+  hash: true,
+  analytics: {
+    baidu: '1ec9adb08b058df906eba988612f4ce3',
+  },
+  favicon: '/assets/favicon.svg',
   ssr: {},
   exportStatic: {},
+  theme: {
+    '@primary-color': '#10B981',
+  },
   nodeModulesTransform: {
     type: 'none',
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,326 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1>Hi i am TailwindCSS China</h1>
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <img className="w-10" src="/assets/logo.svg" />
+            <span className="ml-3 text-xl">TailwindCSS China</span>
+          </a>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 px-3 text-gray-600 text-green-600">主页</a>
+            <a
+              className="mr-5 px-3 text-gray-600"
+              href="https://www.tailwindcss.cn/"
+              target="_blank"
+            >
+              官方文档
+            </a>
+            <a className="mr-5 px-3 text-gray-600">模板商店</a>
+            <a className="mr-5 px-3 text-gray-600">社区博客</a>
+            <a className="mr-5 px-3 text-gray-600 flex items-center">
+              <span>实用工具</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </nav>
+          <img
+            className="w-10 h-10 shadow rounded-full cursor-pointer"
+            src="https://avatars.githubusercontent.com/u/24560160?v=4"
+            alt="avatar"
+          />
+        </div>
+      </header>
+
+      <section className="w-full relative bg-green-500">
+        <img
+          className="w-full h-64 object-cover invisible"
+          src="https://cdn.dribbble.com/assets/searches/search-header-large-c898465b010818ec99c65ebb934655db9dadf0e1c07df82596fee16516fbf61f.webp"
+          alt="post"
+        />
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-white">
+          <h2 className="text-white text-5xl">TailwindCSS Template</h2>
+          <p className="text-lg">
+            7,300,000+ template from thousands of UI developers x designers
+          </p>
+        </div>
+        <div className="absolute w-full -bottom-8 left-0">
+          <div className="m-auto w-1/3 h-16 bg-white shadow-xl rounded-xl flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              role="img"
+              className="mx-8 text-gray-300 fill-current"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M10.6002 12.0498C9.49758 12.8568 8.13777 13.3333 6.66667 13.3333C2.98477 13.3333 0 10.3486 0 6.66667C0 2.98477 2.98477 0 6.66667 0C10.3486 0 13.3333 2.98477 13.3333 6.66667C13.3333 8.15637 12.8447 9.53194 12.019 10.6419C12.0265 10.6489 12.0338 10.656 12.0411 10.6633L15.2935 13.9157C15.6841 14.3063 15.6841 14.9394 15.2935 15.33C14.903 15.7205 14.2699 15.7205 13.8793 15.33L10.6269 12.0775C10.6178 12.0684 10.6089 12.0592 10.6002 12.0498ZM11.3333 6.66667C11.3333 9.244 9.244 11.3333 6.66667 11.3333C4.08934 11.3333 2 9.244 2 6.66667C2 4.08934 4.08934 2 6.66667 2C9.244 2 11.3333 4.08934 11.3333 6.66667Z"
+              ></path>
+            </svg>
+            <input
+              className="border-0 h-full text-lg focus:outline-none placeholder-gray-300"
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full">
+        <div className="container m-auto grid gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 py-20">
+          {Array.from(new Array(21), (v) => (
+            <div className="w-full">
+              <div className="rounded overflow-hidden cursor-pointer">
+                <img
+                  className="w-full shadow transition duration-500 ease-in-out transform hover:scale-110"
+                  src="https://jdc.jd.com/img/600x400"
+                  alt="image"
+                />
+              </div>
+              <div className="flex justify-between items-center pt-4">
+                <div className="flex items-center">
+                  <img
+                    className="w-8 h-8 rounded-full shadow"
+                    src="https://avatars.githubusercontent.com/u/24560160?v=4"
+                    alt="author avatar"
+                  />
+                  <div className="pl-4 text-lg">Turkyden</div>
+                </div>
+                <div className="flex items-center text-gray-500">
+                  <div className="pr-2">v2.1.0</div>
+                  <div className="flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      class="w-4 h-4"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>21</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <footer className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+          <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
+            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+              <img className="w-10" src="/assets/logo.svg" />
+              <span className="ml-3 text-xl">TailwindCSS China</span>
+            </a>
+            <p className="mt-2 text-sm text-gray-500">
+              一个国内最活跃的，资源最丰富的 TailwindCSS 开发者社区
+            </p>
+          </div>
+          <div className="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                CATEGORIES
+              </h2>
+              <nav className="list-none mb-10">
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    First Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Second Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Third Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Fourth Link
+                  </a>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                CATEGORIES
+              </h2>
+              <nav className="list-none mb-10">
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    First Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Second Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Third Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Fourth Link
+                  </a>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                CATEGORIES
+              </h2>
+              <nav className="list-none mb-10">
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    First Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Second Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Third Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Fourth Link
+                  </a>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                CATEGORIES
+              </h2>
+              <nav className="list-none mb-10">
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    First Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Second Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Third Link
+                  </a>
+                </li>
+                <li>
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Fourth Link
+                  </a>
+                </li>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-100">
+          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p className="text-gray-500 text-sm text-center sm:text-left">
+              © 2020 TailwindCSS China —
+              <a
+                href="https://github.com/turkyden"
+                rel="noopener noreferrer"
+                className="text-gray-600 ml-1"
+                target="_blank"
+              >
+                @Turkyden
+              </a>
+            </p>
+            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+              <a className="text-gray-500">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a className="ml-3 text-gray-500">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a className="ml-3 text-gray-500">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
+              <a className="ml-3 text-gray-500">
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="0"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  ></path>
+                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
+
+9;
