37d1134157def870fc580c1310efaa89a1ef9bdb
“turkyden” <wj871287@gmail.com>
2021-09-27 18:33:37 +0800

    fix: update cdn config

--- .umirc.ts
+++ .umirc.ts
@@ -9,12 +9,17 @@ export default defineConfig({
     },
     {
       name: 'description',
-      content: 'TailwindCSS 中文社区',
+      content: '一个国内最活跃的，资源最丰富的 TailwindCSS 开发者社区',
     },
   ],
   // ssr: {},
   // exportStatic: {},
   hash: true,
+  runtimePublicPath: true,
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? 'https://cdn.jsdelivr.net/gh/turkyden/tailwindcss-china@gh-pages/'
+      : '/',
   analytics: {
     baidu: '1ec9adb08b058df906eba988612f4ce3',
   },
